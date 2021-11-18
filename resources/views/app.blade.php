ceeca9abac544e679edf8b37dd69690c417d1070
Johan Hanses <johanhanses@gmail.com>
2021-11-18 20:54:10 +0100

    standard menu transitions/transform

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,24 +8,52 @@
     <title>Stripe menu</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
+          rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src="{{ asset("js/app.js") }}" type="module" defer></script>
 </head>
 
 <body>
-    <header>
-        <nav>
-            <ul>
-                <li><button>Products</button></li>
-                <li><button>Developers</button></li>
-                <li><button>Company</button></li>
-            </ul>
-        </nav>
-    </header>
+<header>
+    <nav>
+        <ul>
+            <li>
+                <button>Products</button>
 
-    <div>
-        HEJE
-    </div>
+                <div class="menu menu-products">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Amet aperiam doloribus dolorum, ea enim et hic mollitia
+                    nulla obcaecati porro quas quis sit sunt. Architecto
+                    asperiores error incidunt nemo reprehenderit?
+                </div>
+            </li>
+            <li>
+                <button>Developers</button>
+
+                <div class="menu menu-developers">
+                    Developers Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Amet aperiam doloribus dolorum, ea enim et hic mollitia
+                    nulla obcaecati porro quas quis sit sunt. Architecto
+                    asperiores error incidunt nemo reprehenderit?
+                </div>
+            </li>
+            <li>
+                <button>Company</button>
+
+                <div class="menu menu-company">
+                    Company Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Amet aperiam doloribus dolorum, ea enim et hic mollitia
+                    nulla obcaecati porro quas quis sit sunt. Architecto
+                    asperiores error incidunt nemo reprehenderit?
+                </div>
+            </li>
+        </ul>
+    </nav>
+</header>
+
+<div>
+    HEJE
+</div>
 </body>
 </html>
