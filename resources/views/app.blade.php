dab5a0c4a8b78844797138da303ea1f93b453c0b
Johan Hanses <johanhanses@gmail.com>
2021-11-18 19:53:22 +0100

    normalize and setup default

---
+++ resources/views/app.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Stripe menu</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset("js/app.js") }}" type="module" defer></script>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><button>Products</button></li>
+                <li><button>Developers</button></li>
+                <li><button>Company</button></li>
+            </ul>
+        </nav>
+    </header>
+
+    <div>
+        HEJE
+    </div>
+</body>
+</html>
